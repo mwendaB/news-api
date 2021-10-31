93f89caf7900b8fa2d921ed2f973f19b1214dc45
mwendaB <brian.mwenda@student.moringaschool.com>
2021-10-31 13:38:01 +0300

    write the request file

--- app/requests.py
+++ app/requests.py
@@ -7,3 +7,9 @@ api_key = None
 base_url = None
 
 articles_url = None
+
+def configure_request(app):
+        global api_key,base_url,articles_url
+        api_key = app.config['NEWS_API_KEY']  
+        base_url = app.config['SOURCE_BASE_URL']
+        articles_url = app.config['ARTICLE_BASE_URL']

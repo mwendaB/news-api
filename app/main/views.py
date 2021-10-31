84d495758cff2c63ea8fd00b9d3f41e3bbb48669
mwendaB <brian.mwenda@student.moringaschool.com>
2021-10-31 13:44:00 +0300

    write the error section

--- app/main/views.py
+++ app/main/views.py
@@ -2,3 +2,25 @@ from flask import render_template,request,redirect,url_for
 from . import main
 from ..requests import get_news_source,get_articles
 from ..models import NewsSource
+
+@main.route('/')
+def index():
+	'''
+	view root page function that returns the index page and its data
+	'''
+	technology = get_news_source('technology')
+	business = get_news_source('business')
+	entertainment = get_news_source('entertainment')
+	title = "Daily News"
+
+	return render_template('index.html',title = title, technology = technology,business = business,entertainment = entertainment)
+
+@main.route('/news_articles/<id>')
+def articles(id):
+	'''
+	view articles  page function that returns the articles page and its data
+	'''
+	articles = get_articles(id)
+	title = f'Daily News articles | {id}'
+
+	return render_template('news_articles.html',title= title,articles = articles)

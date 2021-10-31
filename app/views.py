23e62301da6de19626497dc0824f08691387f24a
mwendaB <brian.mwenda@student.moringaschool.com>
2021-10-31 13:09:44 +0300

    write the py file

--- app/views.py
+++ app/views.py
@@ -2,10 +2,13 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/')
+@app.route('/news/<news_id>')
+def news(news_id):
+
+    
+    return render_template('news.html',id = news_id)
+
 def index():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    return render_template('index.html')
+    title = 'Top of the story'
+    return render_template('index.html', title = title)
